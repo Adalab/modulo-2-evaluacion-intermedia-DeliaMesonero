3ba12e48f79875fdc4f311d9d9711932f96e9d7f
DeliaMesonero <delia_mv88@hotmail.com>
2022-07-06 10:45:52 +0200

    counter

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const input = document.querySelector('.js-input');
 const btn = document.querySelector('.js-btn');
 const clue = document.querySelector('.js-clue');
 let tryNumber = document.querySelector('.js-try');
+let totalCounter = 0;
 
 
 const getRandomNumber = (max) => {
@@ -26,10 +27,28 @@ function number (){
         clue.innerHTML = 'El número debe estar entre 1 y 100';
     }
 }
+function total (btn){
+    totalCounter = totalCounter + btn;
+    return totalCounter;
+    tryNumber.innerHTML = total;
+
+
+}
+let counter = 0;
+function addCounter() {
+  const valueNumber = input.value;
+  if (parseInt(valueNumber) !== parseInt(getRandomNumber)) {
+    counter += 1;
+    tryNumber.innerHTML = `Número de intentos: ${counter}`;
+  }
+  return counter;
+}
 
 function handleClick(ev) {
 ev.preventDefault();
 number();
+total();
+addCounter();
 
     }
 
