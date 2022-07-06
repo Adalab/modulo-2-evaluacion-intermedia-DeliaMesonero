ea3181ccc2a0069290aeb7ad489e09dd3d77c635
DeliaMesonero <delia_mv88@hotmail.com>
2022-07-06 09:08:58 +0200

    listener

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 'use strict'
+console.log('holis');
+
+const input = document.querySelector('.js-input');
+const btn = document.querySelector('.js-btn');
+const clue = document.querySelector('.js-clue');
+const tryNumber = document.querySelector('.js-try');
+
+
+handleClick
+
+
+btn.addEventListener('click', handleClick);
