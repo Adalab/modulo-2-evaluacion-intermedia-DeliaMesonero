790bc87b88b4707270389ac53ba3027a18389278
DeliaMesonero <delia_mv88@hotmail.com>
2022-07-06 11:06:07 +0200

    conditional

--- js/main.js
+++ js/main.js
@@ -11,33 +11,35 @@ let totalCounter = 0;
 const getRandomNumber = (max) => {
     return Math.ceil(Math.random() * max);
     };
-    console.log(getRandomNumber(100));
+    const numberRandom = getRandomNumber(100);
+    console.log(numberRandom);
 
 function number (){
-    const inputValue = input.value;
-    if( inputValue === getRandomNumber(100)) {
+    const inputValue = parseInt (input.value);
+    if( inputValue === numberRandom) {
         clue.innerHTML = 'Has ganado campeona!!';
-    } else if ( inputValue < getRandomNumber(100)) {
+    } else if ( inputValue < 1 || inputValue > 100) {
+            clue.innerHTML = 'El número debe estar entre 1 y 100';
+    } else if ( inputValue < numberRandom) {
         clue.innerHTML ='Demasiado bajo';
 
-    } else if( inputValue > getRandomNumber(100)){
+    } else if ( inputValue < 1 || inputValue > 100) {
+            clue.innerHTML = 'El número debe estar entre 1 y 100';
+
+    } else if( inputValue > numberRandom){
         clue.innerHTML = 'Demasiado alto';
 
-    }else if ( inputValue < 1 && inputValue > 100) {
-        clue.innerHTML = 'El número debe estar entre 1 y 100';
     }
-}
-function total (btn){
-    totalCounter = totalCounter + btn;
-    return totalCounter;
-    tryNumber.innerHTML = total;
+    }
 
 
-}
+
+
+
 let counter = 0;
 function addCounter() {
   const valueNumber = input.value;
-  if (parseInt(valueNumber) !== parseInt(getRandomNumber)) {
+  if (parseInt(valueNumber) !== parseInt(numberRandom)) {
     counter += 1;
     tryNumber.innerHTML = `Número de intentos: ${counter}`;
   }
