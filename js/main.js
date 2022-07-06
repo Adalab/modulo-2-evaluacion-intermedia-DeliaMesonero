f5f40bb42289513a3132c74262c37a85895dc335
DeliaMesonero <delia_mv88@hotmail.com>
2022-07-06 10:23:49 +0200

    conditionals

--- js/main.js
+++ js/main.js
@@ -4,10 +4,33 @@ console.log('holis');
 const input = document.querySelector('.js-input');
 const btn = document.querySelector('.js-btn');
 const clue = document.querySelector('.js-clue');
-const tryNumber = document.querySelector('.js-try');
+let tryNumber = document.querySelector('.js-try');
 
 
-handleClick
+const getRandomNumber = (max) => {
+    return Math.ceil(Math.random() * max);
+    };
+    console.log(getRandomNumber(100));
 
+function number (){
+    const inputValue = input.value;
+    if( inputValue === getRandomNumber(100)) {
+        clue.innerHTML = 'Has ganado campeona!!';
+    } else if ( inputValue < getRandomNumber(100)) {
+        clue.innerHTML ='Demasiado bajo';
+
+    } else if( inputValue > getRandomNumber(100)){
+        clue.innerHTML = 'Demasiado alto';
+
+    }else if ( inputValue < 1 && inputValue > 100) {
+        clue.innerHTML = 'El número debe estar entre 1 y 100';
+    }
+}
+
+function handleClick(ev) {
+ev.preventDefault();
+number();
+
+    }
 
 btn.addEventListener('click', handleClick);
